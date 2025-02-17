ca5eabc40e3fb92b84c5047c377ec8044fbfa4cc
jym126 <jym126@hotmail.com>
2025-02-17 20:10:19 +0100

    contact

--- src/views/ContactView.tsx
+++ src/views/ContactView.tsx
@@ -1,11 +1,7 @@
 import Contact from "@/components/Contact";
-import SpaceComponent from "@/components/SpaceComponent";
 
 export default function ContactView() {
   return (
-    <>
-    <SpaceComponent />
     <Contact/>
-    </>
   )
 }
